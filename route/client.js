e69a527c734baf46dce1d06f886c9f23bf44e220
Maxime Guedj <max.guedj13@gmail.com>
2016-06-21 20:24:23 +0200

    Common lib testing

--- route/client.js
+++ route/client.js
@@ -1,10 +1,14 @@
+const clientModel = require('sharemyscreen-common').Client;
 
 function registerRoute (router) {
   router.get('/client', getClients);
 }
 
 function getClients (req, res, next) {
-  res.send('ok');
+  clientModel.find(function (err, clients) {
+    console.log(clients);
+    res.status(200).json(clients);
+  });
 }
 
 module.exports.registerRoute = registerRoute;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,40 @@
+const devApp = require('../');
+const config = require('config');
+const logger = require('winston');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+
+const url = 'http://dev.sharemyscreen.local:' + config.get('server.port');
+const devSrv = supertest(url);
 
 describe('Testing developper service', function () {
+
+  before(function (done) {
+    mongoose.connection.on('error', function (err) {
+      logger.error('Unable to connect to the database ...');
+      logger.error(err);
+      done(err);
+    });
+
+    mongoose.connection.on('open', function () {
+      logger.info('Connection success !');
+
+      const app = devApp.getApp();
+      app.listen(config.get('server.port'), function () {
+        done();
+      });
+    });
+
+    const connectionStr = 'mongodb://' + config.get('dbConfig.host') + ':' +
+      config.get('dbConfig.port') + '/' +
+      config.get('dbConfig.dbName');
+    mongoose.connect(connectionStr);
+  });
+
+  it('Should work', function (done) {
+    devSrv.get('/v1/client')
+      .expect(200)
+      .end(done);
+  });
 
 });
