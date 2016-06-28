3f0cd7d32da6330aa5e1325707da6df9fccc4d41
Maxime Guedj <max.guedj13@gmail.com>
2016-06-29 00:51:45 +0200

    Allow CROS (via adding headers)

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function getApp () {
 
   devRouter = express.Router();
 
+  devApp.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    next();
+  });
+
   // Register all routes
   client.registerRoute(devRouter);
 
