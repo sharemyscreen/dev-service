5c9884918068341955fb009fa79f839ecb0cae6d
Maxime Guedj <max.guedj13@gmail.com>
2016-06-29 01:13:54 +0200

    Allow CROS (via adding headers)

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function getApp () {
 
   devApp.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
   });
 
